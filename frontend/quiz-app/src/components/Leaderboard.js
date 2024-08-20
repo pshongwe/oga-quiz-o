f68971c0cebd1e8fbe17ab6045d1cb2d5eacc83c
Pibo Shongwe <piboman@yahoo.com>
2024-08-20 22:40:50 +0200

    add leaderboard component

---
+++ frontend/quiz-app/src/components/Leaderboard.js
@@ -0,0 +1,45 @@
+// components/Leaderboard.js
+import React, { useState, useEffect } from 'react';
+import axiosInstance, { getQuizzesApiPath } from '../api/axiosConfig';
+import { useParams } from 'react-router-dom';
+
+function Leaderboard() {
+  const { id } = useParams();
+  const [leaderboard, setLeaderboard] = useState([]);
+
+  useEffect(() => {
+    // Fetch the leaderboard data using the API endpoint
+    axiosInstance.get(getQuizzesApiPath(`/api/v1/leaderboard/${id}`))
+      .then((response) => {
+        setLeaderboard(response.data.leaderboard);
+      })
+      .catch((error) => {
+        // Handle error
+        console.log('failed to fetch leaderboard!');
+      });
+  }, [id]);
+
+  return (
+    <div>
+      <h1>Leaderboard</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Rank</th>
+            <th>Score</th>
+          </tr>
+        </thead>
+        <tbody>
+          {leaderboard.map((entry, index) => (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{entry.score}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Leaderboard;
